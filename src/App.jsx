7e0cb868461d597b062a2a0d15d1812bd4d02c04
dvasquez08 <dvasquez.422@gmail.com>
2023-11-01 18:41:08 -0600

    Updated title of app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <h1>Barcode and QR Code Generator!</h1>
+      <h1>Barcode and QR Code Generator! By David Vasquez</h1>
     </div>
   );
 }
