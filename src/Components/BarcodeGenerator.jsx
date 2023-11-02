fa674c151751923d6cc67474144d470b72043d00
dvasquez08 <dvasquez.422@gmail.com>
2023-11-01 18:47:54 -0600

    Created components for barcode and qr generator

---
+++ src/Components/BarcodeGenerator.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function barcodeGenerator() {
+  return (
+    <>
+      <h2>Barcode Generator</h2>
+    </>
+  );
+}
---
+++ src/Components/QRGenerator.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function qrCodeGenerator() {
+  return (
+    <>
+      <h2>QR Code Generator</h2>
+    </>
+  );
+}
