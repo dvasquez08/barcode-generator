c8ca07ffd63499317e29fb8e4987078c8a0e7a1d
dvasquez08 <dvasquez.422@gmail.com>
2023-11-03 11:54:14 -0600

    Added texts for the barcode and qr code generator

--- src/Components/BarcodeGenerator.jsx
+++ src/Components/BarcodeGenerator.jsx
@@ -5,6 +5,7 @@ function BarcodeGenerator({ value }) {
   return (
     <>
       <h2 className="display-6">Barcode Generator</h2>
+      <p>Your barcode will be generated below:</p>
       <Barcode value={value} />
     </>
   );
--- src/Components/QRGenerator.jsx
+++ src/Components/QRGenerator.jsx
@@ -5,6 +5,7 @@ function QRCodeGenerator({ value }) {
   return (
     <>
       <h2 className="display-6">QR Code Generator</h2>
+      <p>The QR image below will change to your data:</p>
       <QRCode value={value} />
     </>
   );
