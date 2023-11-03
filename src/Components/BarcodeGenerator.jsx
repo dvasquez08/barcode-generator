067ca7a4fc58025f7ce0087191e1d61e9d0770a0
dvasquez08 <dvasquez.422@gmail.com>
2023-11-03 12:46:09 -0600

    Added download functionality for barcode generator

--- src/Components/BarcodeGenerator.jsx
+++ src/Components/BarcodeGenerator.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import Barcode from "react-barcode";
+import html2canvas from "html2canvas";
 
 function BarcodeGenerator({ value }) {
+  const barcodeRef = useRef(null);
+  const handleDownload = () => {
+    html2canvas(barcodeRef.current).then((canvas) => {
+      const image = canvas.toDataURL("image.png");
+      const link = document.createElement("a");
+      link.href = image;
+      link.download = "barcode.png";
+      link.click();
+    });
+  };
+
   return (
     <>
-      <h2 className="display-6">Barcode Generator</h2>
-      <p>Your barcode will be generated below:</p>
-      <Barcode value={value} />
+      <div
+        ref={barcodeRef}
+        className="d-flex flex-column justify-content-center align-items-center text-center"
+      >
+        <h2 className="display-6">Barcode Generator</h2>
+        <p>Your barcode will be generated below:</p>
+        <Barcode value={value} />
+        <button
+          onClick={handleDownload}
+          className="mt-4 btn btn-secondary btn-sm"
+        >
+          Download Barcode
+        </button>
+      </div>
     </>
   );
 }
