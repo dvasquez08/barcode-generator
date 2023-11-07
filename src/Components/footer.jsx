cb4a7c6b72e27a23cd4cb9180c2f50009af3aec4
dvasquez08 <dvasquez.422@gmail.com>
2023-11-06 17:07:28 -0700

    Added foote

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -2,9 +2,26 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer mt-auto py-3 bg-secondary">
+    <footer className="footer mt-5 py-3 bg-secondary container-fluid">
       <div className="container">
-        <p className="text-center text-muted">
+        <p>
+          Click{" "}
+          <a
+            href="https://dvasquez08.github.io"
+            className="text-decoration-none text-warning"
+          >
+            here
+          </a>{" "}
+          for my portfolio. Click{" "}
+          <a
+            href="https://github.com/dvasquez08"
+            className="text-decoration-none text-warning"
+          >
+            here
+          </a>{" "}
+          for my GitHub
+        </p>
+        <p className="text-center">
           &copy; {new Date().getFullYear()} David Vasquez
         </p>
       </div>
